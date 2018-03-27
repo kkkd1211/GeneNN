344c82c643f91eb70e8449c21f08d49ededb94bb
root <root@localhost.localdomain>
2018-03-27 23:05:55 +0800

    0327-2305_240

--- code/main.cpp
+++ code/main.cpp
@@ -14,26 +14,30 @@ int main(int argc,char *argv[])
     int dataNO;
     double errRE;
     sscanf(argv[1],"%d",&dataNO);
-    srand((unsigned)time(NULL));
+    srand((unsigned)((unsigned)time(NULL)*dataaNO+dataNO));
     SynBox box; 
     box.train("my",100000);
-    box.savePara(dataNO);
+    //box.savePara(dataNO);
     //box.set("para/para1.txt");
     errRE=box.test("my",100.0);
-            box.saveTestResult("my",dataNO,errRE);
-    errRE=box.test("wt",100.0);
-            box.saveTestResult("wt",dataNO,errRE);
-    errRE=box.test("w3",100.0);
-            box.saveTestResult("w3",dataNO,errRE);
-    errRE=box.test("B+",100.0);
-            box.saveTestResult("B+",dataNO,errRE);
-    errRE=box.test("B-",100.0);
-            box.saveTestResult("B-",dataNO,errRE);
-    errRE=box.test("N+",100.0);
-            box.saveTestResult("N+",dataNO,errRE);
-    errRE=box.test("N-",100.0);
-            box.saveTestResult("N-",dataNO,errRE);
-    errRE=box.test("T-",100.0);
-            box.saveTestResult("T-",dataNO,errRE);
+    if(errRE<3)
+    {
+        box.savePara(dataNO);
+        box.saveTestResult("my",dataNO,errRE);
+        errRE=box.test("wt",100.0);
+                box.saveTestResult("wt",dataNO,errRE);
+        errRE=box.test("w3",100.0);
+                box.saveTestResult("w3",dataNO,errRE);
+        errRE=box.test("B+",100.0);
+                box.saveTestResult("B+",dataNO,errRE);
+        errRE=box.test("B-",100.0);
+                box.saveTestResult("B-",dataNO,errRE);
+        errRE=box.test("N+",100.0);
+                box.saveTestResult("N+",dataNO,errRE);
+        errRE=box.test("N-",100.0);
+                box.saveTestResult("N-",dataNO,errRE);
+        errRE=box.test("T-",100.0);
+                box.saveTestResult("T-",dataNO,errRE);
+    }
 }
 
